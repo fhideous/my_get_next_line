fd1ee5d20b585a913a91db45dd48fc88a887dfa2
fhideous <fhideous@am-l3.msk.21-school.ru>
2020-11-25 22:33:33 +0300

    gnl v.2(no norm, need to trouble with last \n)

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,19 @@
-#ifndef GNL_GET_NEXT_LINE_H
-# define GNL_GET_NEXT_LINE_H
+//
+// Created by Formy Hideous on 11/25/20.
+//
 
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <unistd.h>
 #include <stdlib.h>
-#include "fcntl.h"
+
+//#include "get_next_line_utils.c"
 
 size_t  ft_strlen(const char *s);
-void    *ft_memcpy(void *dst, const void *src, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
-int     get_next_line(int fd, char **line);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
+int		get_next_line(int fd, char **line);
 
-#endif 
+#endif //GNL_V2_GET_NEXT_LINE_H
